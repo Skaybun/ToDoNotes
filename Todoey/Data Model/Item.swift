4d827cc4cd045bb392bd4b1252b7e9b41726b74a
Ali <128646611+Skaybun@users.noreply.github.com>
2023-05-09 15:32:43 +0300

    Update Item.swift

--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -3,15 +3,13 @@
 //  Todoey
 //
 //  Created by Ali KINU on 28.03.2023.
-//  Copyright © 2023 App Brewery. All rights reserved.
-//
 
 import Foundation
 import RealmSwift
 
 class Item : Object {
     @objc dynamic var title : String = ""
-    @objc dynamic var done : Bool = false                   //note :  Array<Int>() == [Int]()
+    @objc dynamic var done : Bool = false                
     @objc dynamic var dateCreated : Date?
-    let parentCategory = LinkingObjects(fromType: Category.self, property: "items")   // for realtionship items cehck it out!
+    let parentCategory = LinkingObjects(fromType: Category.self, property: "items")   
 }
